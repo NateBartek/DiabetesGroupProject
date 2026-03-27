d9f4b6376748bec3ac3b3cb2bea6129f9963a77d
Caitlin Reid <caitlin.reid@ucalgary.ca>
2026-03-27 13:26:56 -0600

    add confusion matrix to decision tree mode;

--- decisionTree.py
+++ decisionTree.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
+from sklearn.metrics import accuracy_score, classification_report, roc_auc_score,RocCurveDisplay, ConfusionMatrixDisplay
+
 
 diabetes_health_indicators_data = pd.read_csv("diabetes_012_health_indicators_BRFSS2015.csv")
 
@@ -28,3 +30,17 @@ print(classification_report(labels_test, predicted_diabetes,
 print("--- ROC-AUC ---")
 print(f"ROC AUC: {roc_auc_score(labels_test, predicted_diabetes_probability, multi_class='ovr'):.4f}")
 
+#ROC AUC curve
+fig, ax = plt.subplots()
+for i, class_name in enumerate(['Non-Diabetic', 'Pre-Diabetic', 'Diabetic']):
+    RocCurveDisplay.from_predictions(labels_test == i, predicted_diabetes_probability[:, i],
+                                     name=class_name, ax=ax)
+plt.title('ROC Curve - Decision Tree')
+plt.show()
+
+
+
+ConfusionMatrixDisplay.from_predictions(labels_test, predicted_diabetes,
+                                        display_labels=['Non-Diabetic', 'Pre-Diabetic', 'Diabetic'])
+plt.title('Confusion Matrix - Decision Tree')
+plt.show()
